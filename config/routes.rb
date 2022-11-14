c336fb62be12b70b28fcf0d2cba27c3c4f7eccdf
Karen Bala <karenbala@gmail.com>
2022-11-14 17:02:11 -0500

    created user sign up form with devise

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
  
   get '/tools', to: "static_pages#index"
   get '/trails/:id', to: "static_pages#index"
+  get '/users/:id', to: "static_pages#index"
 
   namespace :api do
     namespace :v1 do
       resources :tools, only: [:index]
+      resources :users, only: [:show]
       post 'tools/search', to: 'tools#search'
       
     end
