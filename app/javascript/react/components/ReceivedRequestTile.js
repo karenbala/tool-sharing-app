d101f014cfb1a2bfae3ff5afd07ad231abea26a1
Karen Bala <karenbala@gmail.com>
2022-12-02 12:13:25 -0500

    updated overall user show page styling

--- app/javascript/react/components/ReceivedRequestTile.js
+++ app/javascript/react/components/ReceivedRequestTile.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 const ReceivedRequestTile = (props) => {
   return(
-    <div className='callout'>
-      <p>tuulbox product: {props.received_tool_name} </p>
-      <p>{props.received_tool_product}</p>
-      <p>Owner: {props.received_tool_owner_first_name} {props.received_tool_owner_last_name}</p> 
+    <div className=' about-people' >
+      <div className="about-people-profile" >
+        <img className="profile-image" src="https://i.imgur.com/UPVxPjb.jpg" alt="Kishore Kumar"></img>
+      </div>
+      <div className="about-people-borrower">
+        <p className="borrower-name">Request by {props.received_tool_owner_first_name} {props.received_tool_owner_last_name}</p> 
+        <p class="borrower-location">
+          <i className="fa fa-map-marker" aria-hidden="true"></i>
+          Mumbai, India
+        </p>
+        <p class="owner-tool"><strong>{props.received_tool_product}</strong> is being requested by {props.received_tool_owner_first_name} </p>
+        
+      </div>
     </div>
   )
 }
